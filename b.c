85fdf5ad61c6ca3f352b755ae26932d6d8cedaa5
33caio3 <132357221+33caio3@users.noreply.github.com>
2024-02-22 09:20:25 -0300

    Update b.c

--- b.c
+++ b.c
@@ -3,36 +3,31 @@
 #include <math.h>
 #include <string.h>
 
-    //Caio Vinicius Freire Pereira 20220623
-    //Maria Eduarda Menuzzo 20230848
-    //Nathan Phelipe Gimenes Pinto 20231436
-    //Nicollas Reami 20230868
-
 void login(){
     const char login1[50] = "pipoca";   //cria a string constante login1 e a da o valor "pipoca"
     const int senha  = 1609;    //cria a constante inteira senha e a da o valor 1609
     char login2[50];    //cria a variavel string login2
     int senha2; //cria a variavel inteira senha2
     printf("Digite seu login\n");   //pede ao usuario que digite seu login
-    scanf("%s", &login2);   //le o login do usu·rio e o armazena na variavel login2
+    scanf("%s", &login2);   //le o login do usu√°rio e o armazena na variavel login2
     printf("Digite sua senha\n");   //pede que o usuario informe sua senha
     scanf("%d", &senha2);   //le a senha informada pelo usuario e a armazena na variavel senha2
-    if(strcmp(login1,login2) != 0 || senha != senha2){  //compara o login2 com a constante login e a senha2 com a constante senha, caso o login ou a senha estejam diferentes, o programa È encerrado
+    if(strcmp(login1,login2) != 0 || senha != senha2){  //compara o login2 com a constante login e a senha2 com a constante senha, caso o login ou a senha estejam diferentes, o programa √© encerrado
         printf("Login ou senha incorreto!\n"); //informa ao usuario que esta incorreto
         exit(1);    //finaliza o programa
     }else if(strcmp(login1, login2) == 0 && senha == senha2){   //compara o login2 com a constante login e a senha2 com a constante senha, caso o login e a senha estejam de acordo, o programa segue sendo executado
-        printf("Logado na conta!\n");   //informa ao usuario que ele est· logado na conta
+        printf("Logado na conta!\n");   //informa ao usuario que ele est√° logado na conta
     }
 }
 
 int main(){
-    int i = 0;  //variavel criada para que o "while" seja finalizado caso o usu·rio digite "5"
-    login();            //puxa a funÁ„o de login criada logo acima
-    float depositou,retirado, investimento, rmensal, conta, saque, saldo;  //vari·veis utilizadas no programa
-    int menu;   //vari·vel utilizada para que seja o menu no switch case
-    while (i==0){   //while utilizado para dar continuidade no programa e fazer com que ele seja finalizado caso o usu·rio deseje
-        printf("\nMenu\n1. Deposito\n2. Saque\n3. Saldo\n4. Calculadora de investimento\n5. Sair\n");   //opÁıes do menu
-        scanf("%d", &menu); //le a escolha do usu·rio
+    int i = 0;  //variavel criada para que o "while" seja finalizado caso o usu√°rio digite "5"
+    login();            //puxa a fun√ß√£o de login criada logo acima
+    float depositou,retirado, investimento, rmensal, conta, saque, saldo;  //vari√°veis utilizadas no programa
+    int menu;   //vari√°vel utilizada para que seja o menu no switch case
+    while (i==0){   //while utilizado para dar continuidade no programa e fazer com que ele seja finalizado caso o usu√°rio deseje
+        printf("\nMenu\n1. Deposito\n2. Saque\n3. Saldo\n4. Calculadora de investimento\n5. Sair\n");   //op√ß√µes do menu
+        scanf("%d", &menu); //le a escolha do usu√°rio
         switch (menu){  //switch case utilizado para determinar o que o usuario deseja
             case 1: //caso o usuario digite 1..
                 printf("Digite o valor que deseja depositar em sua conta: ");   //perguntar o valor que o usuario deseja depositar
@@ -53,7 +48,7 @@ int main(){
             case 3: //caso o usuario digite 3...
                 printf("O usuario tem %.2f em sua conta.\n", saldo); //informa ao usuario o valor existente na conta
                 break;  //finaliza o case 3
-            case 4: //caso o usu·rio digite 4...
+            case 4: //caso o usu√°rio digite 4...
                 printf("Digite o investimento: ");  //pergunta ao usuario o valor a ser investido
                 scanf("%f", &investimento); //le o valor a ser investido
                 printf("Digite a rentabilidade mensal em %: "); //pergunta ao usuario qual sera a rentabilidade mensal
@@ -63,11 +58,11 @@ int main(){
                 printf("Total investido : %.2f / Rentabilidade: %.2f / Total: %.2f\n", investimento, (investimento * (rmensal*1.0e-2))*12, investimento + (investimento * (rmensal*1.0e-2))*12);    //retorna ao usuario o total investido, a rentabilidade e o total
                 break;  //finaliza o case 4
             case 5: //caso o usuario digite 5...
-                i++;    //È adicionado +1 a vari·vel i e o programa È encerrado
+                i++;    //√© adicionado +1 a vari√°vel i e o programa √© encerrado
                 break;  //finaliza case 5
-            default:    //caso o usuario escolha uma opÁ„o n„o existente
-                printf("Opcao invalida. Tente novamente.\n"); //caso usu·rio escolha uma opÁ„o diferente das disponibilizadas aparece esta mensagem
+            default:    //caso o usuario escolha uma op√ß√£o n√£o existente
+                printf("Opcao invalida. Tente novamente.\n"); //caso usu√°rio escolha uma op√ß√£o diferente das disponibilizadas aparece esta mensagem
         }
     }
-    return 0;   //retorna caso tenha ocorrido algum erro durante o programa È executado
+    return 0;   //retorna caso tenha ocorrido algum erro durante o programa √© executado
 }
